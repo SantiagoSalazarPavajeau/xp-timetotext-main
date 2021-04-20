085b3ac800626a096643caeefe0e4474f43a97f7
Yvonne <yvonnersz@gmail.com>
2021-04-20 14:16:16 -0700

    Start on money2text

--- money2text.js
+++ money2text.js
@@ -1,8 +1,67 @@
-'use strict'
+"use strict";
 
-function money2text (time) {
+function money2text(time) {
+  let [dollar, cents] = time.split(".");
+  let firstNumber = firstNumberToText(dollar[0]);
+  let unit = convertUnit(dollar);
 
-
+  
 }
 
-module.exports = money2text
+function firstNumberToText(number) {
+  switch (number) {
+    case "1":
+      return "one";
+    case "2":
+      return "two";
+    case "3":
+      return "three";
+    case "4":
+      return "four";
+    case "5":
+      return "five";
+    case "6":
+      return "six";
+    case "7":
+      return "seven";
+    case "8":
+      return "eight";
+    case "9":
+      return "nine";
+  }
+}
+
+function convertTwentyToNinety(dollar) {
+    if (dollar[0] === "2") {
+        return "twenty"
+    } else if (dollar[0] === "3") {
+        return "thirty"
+    } else if (dollar[0] === "4") {
+        return "fourty"
+    } else if (dollar[0] === "5") {
+        return "fifty"
+    } else if (dollar[0] === "6") {
+        return "sixty"
+    } else if (dollar[0] === "7") {
+        return "seventy"
+    } else if (dollar[0] === "8") {
+        return "eighty"
+    } else if (dollar[0] === "9") {
+        return "ninety"
+    }
+}
+
+function convertUnit(dollar) {
+  switch (dollar.length) {
+    case 2:
+        return convertTwentyToNinety(dollar)
+    case 3:
+      return "hundred";
+    case 4:
+      return "thousand";
+    default:
+      return dollar;
+  }
+}
+
+module.exports = money2text;
