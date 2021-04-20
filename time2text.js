5233a8bfa996f03ab8d6d49898421f5dbddb7b4f
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-04-20 14:50:54 -0400

    handle edge cases

--- time2text.js
+++ time2text.js
@@ -15,21 +15,135 @@ function time2text(time) {
   } else if (hour === 12 && minutesString === "00") {
     console.log("noon");
     return "noon";
-  } else if (hour < 12) {
+  }else if (minutesString === "05"){
+    if(hour > 18){
+      console.log("five past", hourConvert, "in the evening");
+      return `five past ${hourConvert} in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("five past", hourConvert,  "in the afternoon");
+      return `five past ${hourConvert} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("five past", hourConvert,  "in the morning");
+      return `five past ${hourConvert} in the morning`;
+    }
+} else if (minutesString === "10"){
+    if(hour > 18){
+      console.log("ten past", hourConvert, "in the evening");
+      return `ten past ${hourConvert} in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("ten past", hourConvert,  "in the afternoon");
+      return `ten past ${hourConvert} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("ten past", hourConvert,  "in the morning");
+      return `ten past ${hourConvert} in the morning`;
+    }
+} else if (minutesString === "15"){
+    if(hour > 18){
+      console.log("quarter past", hourConvert, "in the evening");
+      return `quarter past ${hourConvert} in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("quarter past", hourConvert,  "in the afternoon");
+      return `quarter past ${hourConvert} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("quarter past", hourConvert,  "in the morning");
+      return `quarter past ${hourConvert} in the morning`;
+    }
+}else if (minutesString === "20"){
+    if(hour > 18){
+      console.log("twenty past", hourConvert, "in the evening");
+      return `twenty past ${hourConvert} in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("twenty past", hourConvert,  "in the afternoon");
+      return `twenty past ${hourConvert} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("twenty past", hourConvert,  "in the morning");
+      return `twenty past ${hourConvert} in the morning`;
+    }
+} else if (minutesString === "30"){
+    if(hour > 18){
+      console.log("half past", hourConvert, "in the evening");
+      return `half past ${hourConvert} in the evening`;
+    }else if(hour === 12){// afternoon and quarter past
+        console.log("half past noon");
+        return `half past noon`;
+      }else if(hour > 12){// afternoon and quarter past
+      console.log("half past", hourConvert,  "in the afternoon");
+      return `half past ${hourConvert} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("half past", hourConvert,  "in the morning");
+      return `half past ${hourConvert} in the morning`;
+    }
+}else if (minutesString === "40"){
+    if(hour === 11){// afternoon and quarter past
+        console.log("twenty to noon");
+        return `twenty to noon`;
+    }else if(hour > 18){
+      console.log("twenty to", hourConversionHelper(hour +1), "in the evening");
+      return `twenty to ${hourConversionHelper(hour +1) } in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("twenty to", hourConversionHelper(hour +1),  "in the afternoon");
+      return `twenty to ${hourConversionHelper(hour +1)} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("twenty to", hourConversionHelper(hour +1),  "in the morning");
+      return `twenty to ${hourConversionHelper(hour +1)} in the morning`;
+    }
+}else if (minutesString === "45"){
+    if(hour === 11){// afternoon and quarter past
+        console.log("quarter to midnight");
+        return `quarter to midnight`;
+    }else if(hour > 18){
+      console.log("quarter to", hourConversionHelper(hour +1), "in the evening");
+      return `quarter to ${hourConversionHelper(hour +1) } in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("quarter to", hourConversionHelper(hour +1),  "in the afternoon");
+      return `quarter to ${hourConversionHelper(hour +1)} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("quarter to", hourConversionHelper(hour +1),  "in the morning");
+      return `quarter to ${hourConversionHelper(hour +1)} in the morning`;
+    }
+}else if (minutesString === "50"){
+    if(hour > 18){
+      console.log("ten to", hourConversionHelper(hour +1), "in the evening");
+      return `ten to ${hourConversionHelper(hour +1) } in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("ten to", hourConversionHelper(hour +1),  "in the afternoon");
+      return `ten to ${hourConversionHelper(hour +1)} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("ten to", hourConversionHelper(hour +1),  "in the morning");
+      return `ten to ${hourConversionHelper(hour +1)} in the morning`;
+    }
+}else if (minutesString === "55"){
+    if( hour === 11){
+        console.log("five to noon")
+        return "five to noon"
+    }else if(hour > 18){
+      console.log("five to", hourConversionHelper(hour +1), "in the evening");
+      return `five to ${hourConversionHelper(hour +1) } in the evening`;
+    }else if(hour > 12){// afternoon and quarter past
+      console.log("five to", hourConversionHelper(hour +1),  "in the afternoon");
+      return `five to ${hourConversionHelper(hour +1)} in the afternoon`;
+    }else if(hour < 12){ // morning and quarter past
+      console.log("five to", hourConversionHelper(hour +1),  "in the morning");
+      return `five to ${hourConversionHelper(hour +1)} in the morning`;
+    }
+}else if (hour < 12) {
     console.log(hourConvert, minuteConvert, "in the morning");
-    return `${hourConvert}, ${minuteConvert} in the morning`;
+    return `${hourConvert} ${minuteConvert} in the morning`;
   } else if (hour < 18) {
+
     console.log(hourConvert, minuteConvert, "in the afternoon");
-    return `${hourConvert}, ${minuteConvert} in the afternoon`;
+    return `${hourConvert} ${minuteConvert} in the afternoon`;
   } else if (hour >= 18) {
     console.log(hourConvert, minuteConvert, "in the evening");
-    return `${hourConvert}, ${minuteConvert} in the evening`;
-  }
+    return `${hourConvert} ${minuteConvert} in the evening`;
+  } 
 }
 
 function minuteConversionHelper(minutes) {
   if (minutes <= 19) {
     switch (minutes) {
+      case "00":
+          return "o'clock";
       case "01":
         return "oh one";
       case "02":
@@ -74,42 +188,47 @@ function minuteConversionHelper(minutes) {
 
     switch (minutes[0]) {
       case "2":
-        return "twenty-" + secondMinuteConversionHelper(minutes[1]);
+        return "twenty" + secondMinuteConversionHelper(minutes[1]);
       case "3":
-        return "thirty-" + secondMinuteConversionHelper(minutes[1]);
+        return "thirty" + secondMinuteConversionHelper(minutes[1]);
       case "4":
-        return "forty-" + secondMinuteConversionHelper(minutes[1]);
+        return "fourty" + secondMinuteConversionHelper(minutes[1]);
       case "5":
-        return "fifty-" + secondMinuteConversionHelper(minutes[1]);
+        return "fifty" + secondMinuteConversionHelper(minutes[1]);
     }
+
   }
 }
 
 function secondMinuteConversionHelper(minuteNumber) {  
   switch (minuteNumber) {
+    case "0":
+      return ""
     case "1":
-      return "one";
+      return "-one";
     case "2":
-      return "two";
+      return "-two";
     case "3":
-      return "three";
+      return "-three";
     case "4":
-      return "four";
+      return "-four";
     case "5":
-      return "five";
+      return "-five";
     case "6":
-      return "six";
+      return "-six";
     case "7":
-      return "seven";
+      return "-seven";
     case "8":
-      return "eight";
+      return "-eight";
     case "9":
-      return "nine";
+      return "-nine";
   }
 }
 
 function hourConversionHelper(hour) {
   switch (hour) {
+    case 0:
+      return "twelve";
     case 1:
       return "one";
     case 2:
